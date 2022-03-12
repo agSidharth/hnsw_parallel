8a77522bea56d212dc867240f498b06a869b9c4e
SidharthAgarwal18 <sidharthagarwal2001@gmail.com>
2022-03-12 14:57:06 +0530

    converting code

---
+++ convert.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char* argv[]){
+    assert(argc>2);
+
+    string input_dir = argv[1];
+    string output_dir = argv[2];
+
+    int temp;
+
+    ifstream infil(input_dir+"/max_level.txt",ios::in);
+    fstream outfil(output_dir+"/max_level.bin",ios::out);
+    infil>>temp;
+    outfil.write((char*)&temp,4);
+    infil.close();
+    outfil.close();
+
+    ifstream infil(input_dir+"/ep.txt",ios::in);
+    fstream outfil(output_dir+"/ep.bin",ios::out);
+    infil>>temp;
+    outfil.write((char*)&temp,4);
+    infil.close();
+    outfil.close();
+
+    vector<string> innames  = {"/levels.txt","/index.txt","/indptr.txt","/level_offset.txt","/vect.txt"};
+    vector<string> outnames = {"/levels.bin","/index.bin","/indptr.bin","/level_offset.bin","/vect.bin"};
+
+    for(int idx=0;idx<innames.size();idx++)
+    {
+        ifstream infil(input_dir+innames[idx],ios::in);
+        fstream outfil(output_dir+outnames[idx],ios::out);
+        while(infil>>temp)
+        {
+            outfil.write((char*)&temp,4);
+        }    
+        infil.close();
+        outfil.close();
+    }
+}
